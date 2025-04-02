cbac7bd58cebf61d2ff0c4df55f3038c0302bb02
Sal131r1 <unfoundedfall@gmail.com>
2025-04-01 23:01:54 -0500

    Finished my bookbot - updated to include ability to work with other .txts from the book folder.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words, count_letters
+from stats import count_words, count_letters, sort_stats, print_stats
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,9 +8,20 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    text = get_book_text('books/frankenstein.txt')
-    count_words(text)
-    count_letters(text)
+
+    if len(sys.argv) > 1:
+        print("First argument:", sys.argv[1])
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    directory = "./" + sys.argv[1]
+    text = get_book_text(directory)
+    word_count = count_words(text)
+    character_count = count_letters(text)
+
+    sorted_list = sort_stats(character_count)
+    print_stats(sorted_list, word_count, character_count)
     
 
 if __name__ == '__main__':
--- stats.py
+++ stats.py
@@ -5,6 +5,8 @@ def count_words(text):
     for word in words:
         num_words += 1
 
+    return num_words
+
 def count_letters(text):
     letter_dict = {}
     words = text.lower()
@@ -14,10 +16,27 @@ def count_letters(text):
                 letter_dict[letter] += 1
             else:
                 letter_dict[letter] = 1
-
-    for letter in letter_dict:
-        print(f"'{letter}': {letter_dict[letter]}")
     return letter_dict
 
-def sorted_list(dictionary):
-    list = []
+def sort_on(dict_item):
+    return dict_item['count']
+
+def sort_stats(dictionary):
+    sorted_list = []
+
+    for char, entry in dictionary.items():
+        sorted_list.append({'char': char, 'count': entry})
+
+    sorted_list.sort(reverse=True,key=sort_on)
+    return sorted_list
+
+def print_stats(list, word_count, character_count):
+
+    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {word_count} total words")
+    print("\n--------- Character Count -------\n")
+
+    for entry in list:
+        if entry['char'].isalpha():
+            print(f"{entry['char']}: {entry['count']}\n")
+
+    print("============= END ===============")
